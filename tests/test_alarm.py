e85a975f071ef7e4e33c971887cfb5bd4e7d5b8c
dimanu-py <diegomtz126@gmail.com>
2024-05-25 00:21:32 +0200

    test(alarm): verify alarm is on when tire pressure is below low threshold
    Because right now Alarm is tightly coupled to Sensor, we have use a stub to be able to test isolated the alarm behavior

--- tests/test_alarm.py
+++ tests/test_alarm.py
@@ -1,6 +1,18 @@
+from unittest.mock import patch
+
 from tire_pressure.alarm import Alarm
 
 
-def test_alarm_is_off_by_default():
-    alarm = Alarm()
-    assert not alarm.is_alarm_on
+class TestAlarm:
+
+    def test_alarm_is_off_by_default(self):
+        alarm = Alarm()
+        assert not alarm.is_alarm_on
+
+    def test_alarm_is_on_when_pressure_is_too_low(self):
+        alarm = Alarm()
+
+        with patch("tire_pressure.sensor.Sensor.pop_next_pressure_psi_value", return_value=16):
+            alarm.check_pressure()
+
+            assert alarm.is_alarm_on
