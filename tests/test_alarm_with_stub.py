cb88c4eb1eb25801896b35780b68fa37eb9c0b1b
dimanu-py <diegomtz126@gmail.com>
2024-05-25 10:40:24 +0200

    test(alarm-fake-object): verify alarm is on when pressure is below low threshold using fake object

--- tests/test_alarm_with_stub.py
+++ tests/test_alarm_with_stub.py
@@ -9,3 +9,13 @@ class TestAlarm:
         alarm = Alarm(sensor=stub_sensor)
 
         assert not alarm.is_alarm_on
+
+
+    def test_alarm_is_on_when_pressure_is_too_low(self):
+        stub_sensor = StubSensor()
+        stub_sensor.stub_call_to_pop_next_pressure_psi_value([16])
+        alarm = Alarm(sensor=stub_sensor)
+
+        alarm.check_pressure()
+
+        assert alarm.is_alarm_on
